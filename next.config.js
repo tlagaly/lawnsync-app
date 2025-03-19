15031ca3d4af739bac0eb19741e4aad3fb3ad310
tlagaly <tlagaly@gmail.com>
2025-03-19 00:11:15 -0500

    fix: simplify Next.js and Vercel configuration

--- next.config.js
+++ next.config.js
@@ -8,41 +8,7 @@ const nextConfig = {
   },
   experimental: {
     optimizeCss: true,
-  },
-  webpack: (config) => {
-    // Remove existing CSS rules
-    config.module.rules = config.module.rules.filter(
-      (rule) => !(rule.test && rule.test.test('.css'))
-    );
-
-    // Add our custom CSS rules
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            importLoaders: 1,
-            modules: {
-              auto: true,
-              localIdentName: '[local]_[hash:base64:5]',
-            },
-          },
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            postcssOptions: {
-              config: true, // Use postcss.config.mjs
-            },
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
+  }
 }
 
 module.exports = nextConfig
